e707764d5dd9a9c71c782a1548b02b4f1bb5c20c
VannesEventurers <vannessacaitlin@gmail.com>
2015-09-18 17:02:24 +0530

    Removed Comments For Fork

--- app/src/main/java/com/eventurers/sample/immudisampleappdemo/MainActivity.java
+++ app/src/main/java/com/eventurers/sample/immudisampleappdemo/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        //added menu comments
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
